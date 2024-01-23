5393b5c4a0ff6d802c379647977072a3a387bab3
monikaprotivova <monika.protivova@cleverfarm.ag>
2024-01-23 21:36:51 +0100

    Initial WIP files

---
+++ src/main/java/com/motycka/java/examples/lesson5/WrapperClassesMemberFunctionsExample.java
@@ -0,0 +1,20 @@
+package com.motycka.java.examples.lesson5;
+
+public class WrapperClassesMemberFunctionsExample {
+
+    public static void main(String[] args) {
+
+        Integer i = 10;
+
+        int x = Integer.parseInt("10");
+
+        String str = Integer.toString(10);
+
+        int max = Integer.max(5, 10);
+
+        int min = Integer.min(5, 10);
+
+        int compare = Integer.compare(5, 10);
+    }
+
+}
