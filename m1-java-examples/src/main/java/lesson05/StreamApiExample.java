4322fbee24a18261fa782d6b22b0631a4cbc3472
monikaprotivova <monika.protivova@cleverfarm.ag>
2024-02-23 09:01:20 +0700

    Update lesson05 examples

--- m1-java-examples/src/main/java/lesson05/StreamApiExample.java
+++ m1-java-examples/src/main/java/lesson05/StreamApiExample.java
@@ -3,7 +3,13 @@ package lesson05;
 import common.Data;
 import common.Examples;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 
 public class StreamApiExample {
@@ -12,6 +18,11 @@ public class StreamApiExample {
 
     public static void main(String[] args) {
         filteringExample();
+        sortingExample();
+        mappingExample();
+        reducingExample();
+        groupingExample();
+        otherExamples();
     }
 
     private static void filteringExample() {
@@ -21,6 +32,62 @@ public class StreamApiExample {
                 .filter(item -> item.startsWith("A")) // filters and returns Stream
                 .toList(); // converts Stream to List
 
+        System.out.println("Filtering example:");
         Examples.print(citiesStartingWith);
     }
+
+    private static void sortingExample() {
+
+        List<String> sortedCities = data
+                .stream() // creates Stream
+                .sorted(Comparator.reverseOrder()) // compares using the comparator
+                .toList(); // converts Stream to List
+
+        System.out.println("Sorting example:");
+        Examples.print(sortedCities);
+    }
+
+    private static void mappingExample() {
+        List<String> mappedCities = data
+                .stream() // creates Stream
+                .map(item -> {
+                    // converts to upper case and removes all vowels
+                    return item.toUpperCase().replaceAll("[AEIOUY]", "");
+                }) // compares using the comparator
+                .toList(); // converts Stream to List
+
+        System.out.println("Mapping example:");
+        Examples.print(mappedCities);
+    }
+
+    private static void reducingExample() {
+        int reducedCities = data
+                .stream() // creates Stream
+                .map(String::length) // or mapToInt(String::length)
+                .reduce(0, Integer::sum);
+
+        System.out.println("Reducing example: " + reducedCities);
+    }
+
+    private static void groupingExample() {
+        Map<String, List<String>> groupedCities = data
+                .stream() // creates Stream
+                .collect(Collectors.groupingBy(s -> String.valueOf(s.charAt(0))));
+
+        System.out.println("Grouping example:");
+        Examples.print(groupedCities);
+    }
+
+    private static void otherExamples() {
+
+        var allMatch = data.stream().allMatch(item -> item.length() == 5);
+        var anyMatch = data.stream().anyMatch(item -> item.length() == 5);
+
+        var stream = data.stream().peek(System.out::println);
+        var stream3 = data.stream().limit(3);
+
+        System.out.println("All match: " + allMatch);
+        System.out.println("Any match: " + anyMatch);
+    }
+
 }
