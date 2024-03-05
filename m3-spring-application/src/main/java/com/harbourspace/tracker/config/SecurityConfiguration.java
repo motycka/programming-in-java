2203518c11b7c18f691e84e1670b94c338d3a82f
monikaprotivova <monika.protivova@cleverfarm.ag>
2024-03-05 08:26:02 +0700

    Week3 - lesson12 - fix authorization error handling

--- m3-spring-application/src/main/java/com/harbourspace/tracker/config/SecurityConfiguration.java
+++ m3-spring-application/src/main/java/com/harbourspace/tracker/config/SecurityConfiguration.java
@@ -1,6 +1,8 @@
 package com.harbourspace.tracker.config;
 
 import com.harbourspace.tracker.authorization.AuthorizationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authorization.AuthorizationDecision;
@@ -11,6 +13,8 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
 
+import java.util.Optional;
+
 /**
  * No that this is not standard or common security configuration in Spring Boot. In real world, you would use
  * different authentication and authorization mechanisms, like JWT, OAuth, etc.
@@ -19,6 +23,8 @@ import org.springframework.security.web.access.intercept.RequestAuthorizationCon
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+    private final Logger logger = LoggerFactory.getLogger(SecurityConfiguration.class);
 
     private final AuthorizationService authorizationService;
 
@@ -43,16 +49,30 @@ public class SecurityConfiguration {
     @Bean
     public AuthorizationManager<RequestAuthorizationContext> authorizationManager() {
         return (authentication, context) -> {
-            var authHeader = context.getRequest().getHeader("Authorization");
-            var isGranted = authHeader.startsWith("Basic ");
-
-            if (isGranted) {
-                var userId = Long.parseLong(authHeader.replace("Basic ", ""));
-                authorizationService.setSecurityContext(userId);
+            var useId = getUserIdIfPresent(context);
+            if (useId.isPresent()) {
+                authorizationService.setSecurityContext(useId.get());
+                logger.info("Authorization granted.");
+                return new AuthorizationDecision(true);
+            } else {
+                logger.error("Authorization not granted.");
+                return new AuthorizationDecision(false);
             }
-
-            return new AuthorizationDecision(isGranted);
         };
     }
 
+    private Optional<Long> getUserIdIfPresent(RequestAuthorizationContext context) {
+        var authHeader = context.getRequest().getHeader("Authorization");
+        if (authHeader != null) {
+            try {
+                var userId = Long.parseLong(authHeader.replace("Basic ", ""));
+                return Optional.of(userId);
+            } catch (NumberFormatException e) {
+                logger.error("Authorization header is invalid.");
+                return Optional.empty();
+            }
+        } else {
+            return Optional.empty();
+        }
+    }
 }
